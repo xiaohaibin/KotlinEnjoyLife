3e2ed919cefabf77db66a6780cdc14d5825378cb
xiaohaibin <xiao.haibin05485@westone.com.cn>
2018-11-13 11:54:58 +0800

    优化查看大图用户体验

--- app/src/main/java/com/stx/xhb/enjoylife/ui/adapter/PhotoViewPagerAdapter.kt
+++ app/src/main/java/com/stx/xhb/enjoylife/ui/adapter/PhotoViewPagerAdapter.kt
@@ -1,14 +1,22 @@
 package com.stx.xhb.enjoylife.ui.adapter
 
+import android.app.ProgressDialog.show
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.v4.view.PagerAdapter
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.github.chrisbanes.photoview.OnViewTapListener
 import com.github.chrisbanes.photoview.PhotoView
+import com.stx.xhb.enjoylife.R
 import com.stx.xhb.enjoylife.config.GlideApp
 import java.util.ArrayList
 
@@ -39,12 +47,25 @@ class PhotoViewPagerAdapter(context: Context, imageList: ArrayList<String>):Page
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val photoView = PhotoView(context)
+        val convertView = View.inflate(context, R.layout.item_photoview, null)
+        val photoView = convertView.findViewById<PhotoView>(R.id.photo_view)
+        val progressBar = convertView.findViewById<ProgressBar>(R.id.progress_view)
         photoView.setScaleType(ImageView.ScaleType.FIT_CENTER)
         val imgUrl = imageList[position]
         GlideApp.with(context)
                 .load(imgUrl)
                 .transition(DrawableTransitionOptions.withCrossFade())
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any, target: Target<Drawable>, isFirstResource: Boolean): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(resource: Drawable, model: Any, target: Target<Drawable>, dataSource: DataSource, isFirstResource: Boolean): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+                })
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .into(photoView)
 
@@ -60,8 +81,8 @@ class PhotoViewPagerAdapter(context: Context, imageList: ArrayList<String>):Page
             }
             true
         })
-        container.addView(photoView)
-        return photoView
+        container.addView(convertView)
+        return convertView
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
