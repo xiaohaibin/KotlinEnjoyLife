ea772e2fff5aa0cb37b2e1f626ff7d7ac37642d4
xiaohaibin <xiao.haibin05485@westone.com.cn>
2018-11-22 10:52:36 +0800

    fix bug

--- app/src/main/java/com/stx/xhb/enjoylife/config/Config.kt
+++ app/src/main/java/com/stx/xhb/enjoylife/config/Config.kt
@@ -19,7 +19,7 @@ class Config {
     }
 
     enum class Channel constructor(var title: Int, var icon: Int) {
-        IMAGE(R.string.fragment_image_title, R.drawable.icon_picture_black_24px),
+        IMAGE(R.string.fragment_image_title, R.drawable.ic_photo_black_24dp),
         WALLPAPER(R.string.fragment_wallpaper_title, R.drawable.icon_picture_black_24px),
         VIDEO(R.string.fragment_video_title, R.drawable.ic_video_black_24px),
         ZHIHU(R.string.fragment_zhihu_title, R.drawable.icon_zhihu_black_24px)
